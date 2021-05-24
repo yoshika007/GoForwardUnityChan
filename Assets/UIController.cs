9e9f23eaafd893b2681421c21b7f5bd3c2a73460
yoshika007 <82786157+yoshika007@users.noreply.github.com>
2021-05-24 22:45:41 +0900

    Initial commit

---
+++ Assets/UIController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+
+    //ゲームオーバーテキスト
+    private GameObject gameOberText;
+
+    //走行距離テキスト
+    private GameObject runLengthText;
+
+    //走った距離
+    private float len = 0;
+
+    //走る速度
+    private float speed = 5f;
+
+    //ゲームオーバーの判定
+    private bool isGameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameOberText  = GameObject.Find ("GameOver");
+        this.runLengthText = GameObject.Find("RunLength");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isGameOver == false)
+        {
+
+            //走った距離を更新する
+            this.len += this.speed * Time.deltaTime;
+
+            //走った距離を表示する
+            this.runLengthText.GetComponent<Text>().text = "Distance:  " + len.ToString("F2") + "m";
+
+        }
+
+        //ゲームオーバーになった場合
+        if (this.isGameOver == true)
+        {
+            //クリックされたらシーンをロードする
+            if (Input.GetMouseButtonDown(0))
+            {
+                //SampleSceneを読み込む
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+
+    }
+
+    public void GameOver()
+    {
+        //ゲームオーバーになった時、画面上にゲームオーバーを表示する
+        this.gameOberText.GetComponent<Text>().text = "Game Over";
+        this.isGameOver = true;
+    }
+
+}
